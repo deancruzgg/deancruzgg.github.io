fdbc090800a50aa51fdb55485ccf00233b70f6d8
Dean Cruz <cruzdean1997@gmail.com>
2023-11-11 21:53:10 +0800

    Add parallax effect to hero

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,43 @@
 //
 // Scripts
 // 
+
+
 window.addEventListener('DOMContentLoaded', event => {
+    //Parallax
+    const parallax_el = document.querySelectorAll(".parallax");
+    let xValue = 0, yValue = 0;
+
+    let rotateDegree = 0;
+
+    function updateParallax(cursorPosition) {
+        parallax_el.forEach(el => {
+            let speedx = el.dataset.speedx;
+            let speedy = el.dataset.speedy;
+            let speedz = el.dataset.speedz;
+            let rotateSpeed = el.dataset.rotation;
+    
+            let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+            let zValue = cursorPosition - parseFloat(getComputedStyle(el).left) * isInLeft;
+    
+            el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px))
+             rotateY(${rotateDegree * rotateSpeed}deg)
+             translateY(calc(-50% + ${yValue * speedy}px)) 
+             perspective(2300px) translateZ(${zValue * speedz}px)`;
+        });
+    };
+
+    updateParallax(0);
+
+    window.addEventListener("mousemove", (e) => {
+        xValue = e.clientX - window.innerWidth / 2;
+        yValue = e.clientY - window.innerHeight / 2;
+
+        rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+        updateParallax(e.clientX);
+    });
+
 
     // Navbar shrink function
     var navbarShrink = function () {
