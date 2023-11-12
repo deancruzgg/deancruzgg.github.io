a418421f39d0f7595d3e9c1684c06456a77c2e4c
Dean Cruz <cruzdean1997@gmail.com>
2023-11-12 22:55:16 +0800

    Finish chatbot

--- js/chatbot/chatbot.js
+++ js/chatbot/chatbot.js
@@ -1,5 +1,5 @@
-// Button Collapse
 var collapsible = document.getElementsByClassName("chatbot-collapsible");
+var hasBotMessageExecuted = false;
 
 for (let i = 0; i < collapsible.length; i++) {
   collapsible[i].addEventListener("click", function () {
@@ -10,6 +10,13 @@ for (let i = 0; i < collapsible.length; i++) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+
+      if (!hasBotMessageExecuted) {
+        setTimeout(function () {
+          firstBotMessage();
+          hasBotMessageExecuted = true;
+        }, 1000);
+      }
     }
   });
 }
@@ -30,16 +37,48 @@ function getTime() {
 }
 
 function firstBotMessage() {
-  let firstMessage = "Hello, how are you today?";
+  let firstMessage = "Hey there, I'm a bot, here to assist you in navigating this website, or answering any questions you might have!";
   document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
   let time = getTime();
 
   $("#chat-timestamp").append(time);
+
+  setTimeout(function () {
+    let secondMessage = '<p class="botText"><span>' + 'I can give you more information if you type in the number corresponding to each topic:' + '</span></p>';
+    $("#chatbox").append(secondMessage);
+
+    setTimeout(function () {
+      let thirdMessage = '<p class="botText"><span>' + '1 - Who am I?' + '</span></p>';
+      $("#chatbox").append(thirdMessage);
+
+      setTimeout(function () {
+        let fourthMessage = '<p class="botText"><span>' + '2 - What kind of skills do I have?' + '</span></p>';
+        $("#chatbox").append(fourthMessage);
+
+        setTimeout(function () {
+          let fifthMessage = '<p class="botText"><span>' + '3 - What kind of projects have I done?' + '</span></p>';
+          $("#chatbox").append(fifthMessage);
+          document.getElementById("userInput").scrollIntoView(true);
+
+          setTimeout(function () {
+            let sixthMessage = '<p class="botText"><span>' + '4 - Am I open to collaborations?' + '</span></p>';
+            $("#chatbox").append(sixthMessage);
+            document.getElementById("userInput").scrollIntoView(true);
+
+            setTimeout(function () {
+              let seventhMessage = '<p class="botText"><span>' + '5 - Ways to contact me' + '</span></p>';
+              $("#chatbox").append(seventhMessage);
+              document.getElementById("userInput").scrollIntoView(true);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+
   document.getElementById("userInput").scrollIntoView(false);
 }
-
-firstBotMessage();
 
 function getHardResponse(userText) {
   let botResponse = getBotResponse(userText);
@@ -80,10 +119,10 @@ function sendButton() {
 }
 
 function heartButton() {
-  buttonSendText("Heart clicked.");
+
 }
 
-$("#textInput").keypress(function(e) {
+$("#textInput").keypress(function (e) {
   if (e.which == 13)
     getResponse();
 });
--- js/chatbot/responses.js
+++ js/chatbot/responses.js
@@ -1,13 +1,19 @@
 function getBotResponse(input) {
-  if (input == "1") 
-    return "You typed 1!";
+  if (input == "1")
+    return "My name's Dean, and I'm aspiring to be a software engineer or a front-end developer in the future! Currently I'm a student pursuing a Diploma in Digital Design and Development, after which i'll likely continue my education towards the University level. My hobbies include playing video games, watching movies and finding ways to make my websites look cooler!";
 
   else if (input == "2")
-    return "You typed 2!";
+    return "I'm able to write programs using PHP, Java and Python, as well as create interactive and aesthetically pleasing websites using CSS and Javascript. I'm also able to design intuitive and visually appealing user interfaces with Photoshop, Illustrator and XD. With all of these skills, I'm able to create well-rounded and engaging digital experiences for a wide number of users.";
 
   else if (input == "3")
+    return "I've been able to create interactive websites, design interfaces, assets and prototypes for mobile applications, as well as create mobile applications themselves. You can view some of those projects in this website, or if you'd like to know more, you can drop me a message.";
+
+  else if (input == "4")
+    return "Always! I'm always looking for ways where I can express myself through coding and help others in the process. If you're interested in my skills, don't hesitate to contact me!";
+
+  else if (input == "5")
     return 'You can view the ways you can contact me by going to the ' + '<a href="#contact" class="chat-link">Contact</a> ' + 'section below!';
 
   else
-    return "Sorry, I couldn't understand that. Please try again.";
+    return "Sorry, I couldn't understand that. Please type in a number from 1 to 5.";
 }
