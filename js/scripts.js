ac04d6e27036204c39247374bdaac7a14667e5b4
Dean Cruz <cruzdean1997@gmail.com>
2023-11-14 18:23:46 +0800

    Fix hamburger menu errors

--- js/scripts.js
+++ js/scripts.js
@@ -65,6 +65,15 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Shrink the navbar 
     navbarShrink();
+    
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
 
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
