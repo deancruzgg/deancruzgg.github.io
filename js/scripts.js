4730bc0633b67a65af4905d7bdc855b59523fa06
deancruz1997 <cruzdean1997@gmail.com>
2023-11-16 10:30:16 +0800

    Revert "Minor optimization for mobile devices"
    This reverts commit 135a6d51b28d9b02cad1b1bedf4301435a162069.

--- js/scripts.js
+++ js/scripts.js
@@ -11,33 +11,42 @@ circles.forEach(function (circle, index) {
     circle.style.backgroundColor = colors[index % colors.length];
 });
 
-window.addEventListener("mousemove", function (e) {
-    coords.x = e.clientX;
-    coords.y = e.clientY;
-});
+let isTouchDevice = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
 
-function animateCircles() {
-    let x = coords.x;
-    let y = coords.y;
-
-    circles.forEach(function (circle, index) {
-        circle.style.left = x - 12 + "px";
-        circle.style.top = y - 12 + "px";
-
-        circle.style.transform = "scale(" + (circles.length - index) / circles.length + ")";
-
-        circle.x = x;
-        circle.y = y;
-
-        const nextCircle = circles[index + 1] || circles[0];
-        x += (nextCircle.x - x) * 0.3;
-        y += (nextCircle.y - y) * 0.3;
+if (!isTouchDevice) {
+    window.addEventListener("mousemove", function (e) {
+        coords.x = e.clientX;
+        coords.y = e.clientY;
     });
 
-    requestAnimationFrame(animateCircles);
-}
+    function animateCircles() {
+        let x = coords.x;
+        let y = coords.y;
 
-animateCircles();
+        circles.forEach(function (circle, index) {
+            circle.style.left = x - 12 + "px";
+            circle.style.top = y - 12 + "px";
+
+            circle.style.transform = "scale(" + (circles.length - index) / circles.length + ")";
+
+            circle.x = x;
+            circle.y = y;
+
+            const nextCircle = circles[index + 1] || circles[0];
+            x += (nextCircle.x - x) * 0.3;
+            y += (nextCircle.y - y) * 0.3;
+        });
+
+        requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
+} else {
+    // Hide the circles on touch devices
+    circles.forEach(function (circle) {
+        circle.style.display = "none";
+    });
+}
 
 window.addEventListener('DOMContentLoaded', event => {
     // Navbar shrink function
@@ -56,7 +65,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Shrink the navbar 
     navbarShrink();
-
+    
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
